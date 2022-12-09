e1a783fc4ad07e9c7fe90d71078bfb9c877633b2
BrendenKingTLG <Brenden.king@tlgcohort.com>
2022-12-09 12:07:49 -0600

    moved code around

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,14 +1,15 @@
 public class Game {
 
-    private static final String ANSI_RED = "\u001B[31m";
-    private static final String ANSI_RESET = "\u001B[0m";
-//    private static final String ANSI_GREEN = "\u001B[32m";
-//    private static final String ANSI_YELLOW = "\u001B[33m";
-    private static final String ANSI_BLUE = "\u001B[34m";
 //    private static final String ANSI_PURPLE = "\u001B[35m";
 //    private static final String ANSI_CYAN = "\u001B[36m";
 //    private static final String ANSI_WHITE = "\u001B[37m";
 //    private static final String ANSI_BLACK = "\u001B[30m";
+//    private static final String ANSI_GREEN = "\u001B[32m";
+//    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+
 
     public void gameIntro() {
         //create game intro logo and intro story lines
@@ -25,7 +26,8 @@ public class Game {
                 "                                                                                                         "
                 + ANSI_RESET;
         String gameIntroLogoSubtitle = ANSI_BLUE + "\t\t\t\t\t\t\t\tThe Last Tweet: A Twitter Survival Game" + ANSI_RESET;
-        String storyIntro = "\nIt is October 27th.You wake up at your desk, groggy from an attack.You look around you. There are pink slips everywhere. Fires raging." + "\n" +
+        String storyIntro =
+                "\nIt is October 27th.You wake up at your desk, groggy from an attack.You look around you. There are pink slips everywhere. Fires raging." + "\n" +
                 "Employees crying.Elon Musk has acquired Twitter.Half of the Twitter workforce has been laid off.You are one of the survivors. \n" +
                 "You look at your desk. On your laptop, you have X lines of code. \n" +
                 "What would you like to do? \n" +
