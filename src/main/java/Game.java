2607e3f4e859619c2a04d87bc438b928a6a0d945
BrendenKingTLG <Brenden.king@tlgcohort.com>
2022-12-09 11:14:22 -0600

    added game over method

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,6 @@
 public class Game {
 
-//    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_RED = "\u001B[31m";
     private static final String ANSI_RESET = "\u001B[0m";
 //    private static final String ANSI_GREEN = "\u001B[32m";
 //    private static final String ANSI_YELLOW = "\u001B[33m";
@@ -11,8 +11,8 @@ public class Game {
 //    private static final String ANSI_BLACK = "\u001B[30m";
 
     public void gameIntro() {
-        //create game logo and intro story lines
-        String gameLogo = ANSI_BLUE +
+        //create game intro logo and intro story lines
+        String gameIntroLogo = ANSI_BLUE +
                 "▄▄▄█████▓ ██░ ██ ▓█████     ██▓     ▄▄▄        ██████ ▄▄▄█████▓   ▄▄▄█████▓ █     █░▓█████ ▓█████ ▄▄▄█████▓\n" +
                 "▓  ██▒ ▓▒▓██░ ██▒▓█   ▀    ▓██▒    ▒████▄    ▒██    ▒ ▓  ██▒ ▓▒   ▓  ██▒ ▓▒▓█░ █ ░█░▓█   ▀ ▓█   ▀ ▓  ██▒ ▓▒\n" +
                 "▒ ▓██░ ▒░▒██▀▀██░▒███      ▒██░    ▒██  ▀█▄  ░ ▓██▄   ▒ ▓██░ ▒░   ▒ ▓██░ ▒░▒█░ █ ░█ ▒███   ▒███   ▒ ▓██░ ▒░\n" +
@@ -24,7 +24,7 @@ public class Game {
                 "          ░  ░  ░   ░  ░       ░  ░      ░  ░      ░                           ░       ░  ░   ░  ░         \n" +
                 "                                                                                                         "
                 + ANSI_RESET;
-        String logoSubTitle = ANSI_BLUE + "\t\t\t\t\t\t\t\tThe Last Tweet: A Twitter Survival Game" + ANSI_RESET;
+        String gameIntroLogoSubtitle = ANSI_BLUE + "\t\t\t\t\t\t\t\tThe Last Tweet: A Twitter Survival Game" + ANSI_RESET;
         String storyIntro = "\nIt is October 27th.You wake up at your desk, groggy from an attack.You look around you. There are pink slips everywhere. Fires raging." + "\n" +
                 "Employees crying.Elon Musk has acquired Twitter.Half of the Twitter workforce has been laid off.You are one of the survivors. \n" +
                 "You look at your desk. On your laptop, you have X lines of code. \n" +
@@ -33,9 +33,30 @@ public class Game {
                 "(or enter q to quit)";
 
         //display intro to user
-        System.out.println(gameLogo);
-        System.out.println(logoSubTitle);
+        System.out.println(gameIntroLogo);
+        System.out.println(gameIntroLogoSubtitle);
         System.out.println(storyIntro);
 
+    }
+
+    public void gameOver(){
+        //create game over logo
+        String gameOverLogo = "\n\n" + ANSI_RED +
+                "  ▄████  ▄▄▄       ███▄ ▄███▓▓█████     ▒█████   ██▒   █▓▓█████  ██▀███  \n" +
+                " ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀    ▒██▒  ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒\n" +
+                "▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▒███      ▒██░  ██▒ ▓██  █▒░▒███   ▓██ ░▄█ ▒\n" +
+                "░▓█  ██▓░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄    ▒██   ██░  ▒██ █░░▒▓█  ▄ ▒██▀▀█▄  \n" +
+                "░▒▓███▀▒ ▓█   ▓██▒▒██▒   ░██▒░▒████▒   ░ ████▓▒░   ▒▀█░  ░▒████▒░██▓ ▒██▒\n" +
+                " ░▒   ▒  ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░   ░ ▒░▒░▒░    ░ ▐░  ░░ ▒░ ░░ ▒▓ ░▒▓░\n" +
+                "  ░   ░   ▒   ▒▒ ░░  ░      ░ ░ ░  ░     ░ ▒ ▒░    ░ ░░   ░ ░  ░  ░▒ ░ ▒░\n" +
+                "░ ░   ░   ░   ▒   ░      ░      ░      ░ ░ ░ ▒       ░░     ░     ░░   ░ \n" +
+                "      ░       ░  ░       ░      ░  ░       ░ ░        ░     ░  ░   ░     \n" +
+                "                                                     ░                 "
+                + ANSI_RESET;
+        String gameOverLogoSubtitle = ANSI_RED+ "\tYou lost your employability .....you were fired on the spot" + ANSI_RESET;
+
+        //display game over to user
+        System.out.println(gameOverLogo);
+        System.out.println(gameOverLogoSubtitle);
     }
 }
