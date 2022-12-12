71d849ee15ccf3de3378897b141ab8f7f0c8f601
himantik <himantik@yahoo.com>
2022-12-12 17:12:34 -0500

    Started EnemyTest

---
+++ test/EnemyTest.java
@@ -0,0 +1,54 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EnemyTest {
+
+  @Test
+  public void getNormalAttackDmg() {
+  }
+
+  @Test
+  public void setNormalAttackDmg() {
+  }
+
+  @Test
+  public void getSuperAttackDmg() {
+  }
+
+  @Test
+  public void setSuperAttackDmg() {
+  }
+
+  @Test
+  public void getHealth() {
+  }
+
+  @Test
+  public void setHealth() {
+  }
+
+  @Test
+  public void getTitle() {
+  }
+
+  @Test
+  public void setTitle() {
+  }
+
+  @Test
+  public void getNormalAttack() {
+  }
+
+  @Test
+  public void setNormalAttack() {
+  }
+
+  @Test
+  public void getSuperAttack() {
+  }
+
+  @Test
+  public void setSuperAttack() {
+  }
+}
