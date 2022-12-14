cceba61330f78fc5bb545b5df527e6be7c51b565
BrendenKingTLG <Brenden.king@tlgcohort.com>
2022-12-14 09:45:18 -0600

    added new jar and changed title screen, AB#331

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         //create new game
         while (true) {
             final Player player = new Player(25, 25, 25);
