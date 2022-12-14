80aa2c45d6fedb117af1a8872efacee84991df35
BrendenKingTLG <Brenden.king@tlgcohort.com>
2022-12-14 17:21:37 -0600

    added vending machine and player inventory, AB#322

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.Map;
+
 public class Room {
     private final int north;
     private final int south;
@@ -5,18 +8,30 @@ public class Room {
     private final int east;
     private final String name;
     private final String description;
+    private List<Map<String, Map<String, Integer>>> storage;
+
 
     //create room for game
-    public Room(String name, String description, int mapNorth, int mapSouth, int mapWest, int mapEast) {
+    public Room(String name, String description, int mapNorth, int mapSouth, int mapWest, int mapEast, List<Map<String, Map<String, Integer>>> storage) {
         this.name = name;
         this.description = description;
         this.north = mapNorth;
         this.south = mapSouth;
         this.west = mapWest;
         this.east = mapEast;
+        this.storage = storage;
     }
 
+
     // accessor methods
+    public List<Map<String, Map<String, Integer>>> getStorage() {
+        return storage;
+    }
+
+    public void setStorage(List<Map<String, Map<String, Integer>>> storage) {
+        this.storage = storage;
+    }
+
     public int getNorth() {
         return north;
     }
