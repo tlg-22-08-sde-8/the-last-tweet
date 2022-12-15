c6c2525e2a3c019c3502ca2e2bcb4962bc234b57
Patrick Lauer <bluedestiny88@gmail.com>
2022-12-15 13:07:47 -0800

    Fixed the field, AB#348

--- src/main/java/GameMaps.java
+++ src/main/java/GameMaps.java
@@ -1,71 +1,84 @@
 public class GameMaps {
 
+    private static ArrayList<String> gameMap;
     /**
      *  ASCII of the Work station room
      */
-    private static String workStationRoom =
-            "┌────────────────┐\n" +
-                    "│                │\n" +
-                    "│                │\n" +
-                    "│  Work Station  │\n" +
-                    "│                │\n" +
-                    "│                │\n" +
-                    "└────────────────┘";
+    private static String r1_Workstation = "┌────────────────┐";
+    private static String r2_Workstation = "│                │";
+    private static String r3_Workstation = "│                │";
+    private static String r4_Workstation = "│  Work Station  │";
+    private static String r5_Workstation = "│                │";
+    private static String r6_Workstation = "│                │";
+    private static String r7_Workstation = "└────────────────┘";
 
 
     /**
      * ASCII Extended representation of the Meeting Room
      */
-    private static String meetingRoom =
-            "┌────────────────┐\n" +
-                    "│                │\n" +
-                    "│                │\n" +
-                    "│  Meeting Room  │\n" +
-                    "│                │\n" +
-                    "│                │\n" +
-                    "└────────────────┘";
-
-    /**
-    private static String breakRoom =
-            "┌────────────────┐\n" +
-                    "│                │\n" +
-                    "│                │\n" +
-                    "│   Break Room   │\n" +
-                    "│                │\n" +
-                    "│                │\n" +
-                    "└────────────────┘";
-
-    private static String coffeeBarRoom =
-            "┌────────────────┐\n" +
-                    "│                │\n" +
-                    "│                │\n" +
-                    "│  Coffee Bar    │\n" +
-                    "│                │\n" +
-                    "│                │\n" +
-                    "└────────────────┘";
-
-    private static String emptyWorkstationRoom =
-            "┌────────────────┐\n" +
-                    "│                │\n" +
-                    "│     Empty      │\n" +
-                    "│                │\n" +
-                    "│  Workstation   │\n" +
-                    "│                │\n" +
-                    "└────────────────┘";
+    private static String r1_MeetingRoom = "┌────────────────┐";
+    private static String r2_MeetingRoom = "│                │";
+    private static String r3_MeetingRoom = "│                │";
+    private static String r4_MeetingRoom = "│  Meeting Room  │";
+    private static String r5_MeetingRoom = "│                │";
+    private static String r6_MeetingRoom = "│                │";
+    private static String r7_MeetingRoom = "└────────────────┘";
 
 
-    private static String horizontalArrows =
-            "┌────────────────►\n" +
-                    "◄────────────────┘";
+    private static String r1_BreakRoom = "┌────────────────┐";
+    private static String r2_BreakRoom = "│                │";
+    private static String r3_BreakRoom = "│                │";
+    private static String r4_BreakRoom = "│   Break Room   │";
+    private static String r5_BreakRoom = "│                │";
+    private static String r6_BreakRoom = "│                │";
+    private static String r7_BreakRoom = "└────────────────┘";
 
-    private static String verticalArrows =
-            "┌▲\n" +
+    private static String r1_CoffeeBar = "┌────────────────┐";
+    private static String r2_CoffeeBar = "│                │";
+    private static String r3_CoffeeBar = "│                │";
+    private static String r4_CoffeeBar = "│  Coffee Bar    │";
+    private static String r5_CoffeeBar = "│                │";
+    private static String r6_CoffeeBar = "│                │";
+    private static String r7_CoffeeBar = "└────────────────┘";
+
+    private static String r1_EmptyWorkstation = "┌────────────────┐";
+    private static String r2_EmptyWorkstation = "│                │";
+    private static String r3_EmptyWorkstation = "│     Empty      │";
+    private static String r4_EmptyWorkstation = "│                │";
+    private static String r5_EmptyWorkstation = "│  Workstation   │";
+    private static String r6_EmptyWorkstation = "│                │";
+    private static String r7_EmptyWorkstation = "└────────────────┘";
+
+
+    private static String r1_HorizontalArrows = "                  ";
+    private static String r2_HorizontalArrows = "                  ";
+    private static String r3_HorizontalArrows = "┌────────────────►";
+    private static String r4_HorizontalArrows = "                  ";
+    private static String r5_HorizontalArrows = "◄────────────────┘";
+    private static String r6_HorizontalArrows = "                  ";
+    private static String r7_HorizontalArrows = "                  ";
+
+    private static String r1_VerticalArrows = "         ┌▲\n" +
                     "││\n" +
                     "││\n" +
                     "││\n" +
                     "││\n" +
                     "││\n" +
                     "▼┘";
+
+
+
+     public GameMaps() {
+        gameMap = new ArrayList<>();
+        gameMap.add(workStationRoom);
+        gameMap.add(meetingRoom);
+        gameMap.add(breakRoom);
+        gameMap.add(coffeeBarRoom);
+        gameMap.add(emptyWorkstationRoom);
+        gameMap.add(horizontalArrows);
+        gameMap.add(verticalArrows);
+     }
+
 }
 
 
