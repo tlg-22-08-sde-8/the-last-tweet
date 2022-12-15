791a8f0ee4dedc5ad0c08875e652f27ecbfdc251
himantik <himantik@yahoo.com>
2022-12-15 11:54:35 -0500

    Finished Player test, AB#475

---
+++ test/GameTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest {
+  Game game;
+
+  @Before
+  public void setUp() throws Exception {
+    Player player = new Player(10, 10,10);
+    game = new Game(player);
+  }
+
+  @Test
+  public void travel() {
+  }
+
+  @Test
+  public void parseCommand() throws IOException {
+  }
+}
