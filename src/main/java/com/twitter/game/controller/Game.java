3db27272a4a8b4b4b8cd880e745d543f9dc5e736
BrendenKingTLG <Brenden.king@tlgcohort.com>
2022-12-16 14:06:26 -0600

    adjusting music, AB#334

--- src/main/java/com/twitter/game/controller/Game.java
+++ src/main/java/com/twitter/game/controller/Game.java
@@ -99,7 +99,7 @@ public class Game {
             clip = AudioSystem.getClip();
             clip.open(audioStream);
             FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-            gainControl.setValue(20f * (float) Math.log10(.9));
+            gainControl.setValue(20f * (float) Math.log10(.7));
             clip.start();
         }
     }
