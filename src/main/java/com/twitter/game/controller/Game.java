57b2a52b82b56c21da09d8099c295a65a9eb0434
BrendenKingTLG <Brenden.king@tlgcohort.com>
2022-12-19 17:17:29 -0600

    added music class, AB#585

--- src/main/java/com/twitter/game/controller/Game.java
+++ src/main/java/com/twitter/game/controller/Game.java
@@ -11,15 +11,10 @@ import org.bson.Document;
 
 import javax.sound.sampled.*;
 import java.io.*;
-import java.net.URL;
 import java.util.*;
 
 public class Game {
     private static final String ANSI_PURPLE = "\u001B[35m";
-//    private static final String ANSI_CYAN = "\u001B[36m";
-//    private static final String ANSI_WHITE = "\u001B[37m";
-//    private static final String ANSI_BLACK = "\u001B[30m";
-//    private static final String ANSI_GREEN = "\u001B[32m";
     private static final String ANSI_YELLOW = "\u001B[33m";
     private static final String ANSI_RED = "\u001B[31m";
     private static final String ANSI_RESET = "\u001B[0m";
